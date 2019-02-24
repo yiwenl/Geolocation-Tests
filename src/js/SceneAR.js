1c333a3273a141f9b3d00ed4a37069396e84f422
wen <bongiovi015@gmail.com>
2019-02-24 20:44:07 +0000

    update

--- src/js/SceneAR.js
+++ src/js/SceneAR.js
@@ -1,19 +1,238 @@
 // SceneAR.js
 import alfrid from 'alfrid';
 import Config from './Config';
-import GLTFLoader from 'three-gltf-loader';
-import HeadingCalibrate from './utils/HeadingCalibrate';
-import Device from './Device';
-import DebugInfo from './debug/DebugInfo';
-import { loadModel, placeObjectInfront } from './utils';
+
+const Random = require('canvas-sketch-util/random');
+const { linspace, clamp01 } = require('canvas-sketch-util/math');
+const { quat, vec3 } = require('gl-matrix');
+const palettes = require('color-hunt-scrape');
+const eases = require('eases');
+const objectAssign = require('object-assign');
 
 
-const purple = 0xAD50FF
-const cherry = 0xDD0065
-const mint = 0x00EDAF
-const canary = 0xFCEE21
+const RESOLUTION = 0.75;
+const ONE_INCH = 0.0254;
+const flowerCount = 30;
+const SEED = '491239'; // set to empty string for randomness
+
+Random.setSeed(SEED || Random.getRandomSeed());
+console.log('Seed', Random.getSeed());
+const monochromatic = false;
+const settings = {
+  duration: 5,
+  suffix: Random.getSeed(),
+  units: 'px',
+  // pixelsPerInch: 300,
+  // Make the loop animated
+  animate: true,
+  // Get a WebGL canvas rather than 2D
+  context: 'webgl',
+  // Turn on MSAA
+  attributes: { antialias: true }
+};
+
+
+// const ARUtils = require('./ar/ARUtils')
+const axis = [ 0, 0, 0 ];
 
 const AVG_HUMAN_HEIGHT = 1.65; //	in meters
+
+function createMaterial (opt = {}) {
+
+  const o = {};
+  objectAssign(o, opt);
+  // const emissive = new THREE.Color(opt.color);
+  const material = new THREE.MeshBasicMaterial(o);
+  return material;
+}
+
+function createFlower (opts = {}) {
+  // let monochromatic = window.monochromatic || false;
+  let {
+    bloom = 1,
+    petalCount = 5,
+    size = 1,
+    color = new THREE.Color('red'),
+    stamenColor = new THREE.Color('yellow'),
+    position = new THREE.Vector3(0, 0, 0),
+    normal = new THREE.Vector3(0, 1, 0)
+  } = opts;
+
+  const group = new THREE.Group();
+
+  const seed = String(Random.rangeFloor(0, 1000000));
+  const random = Random.createRandom(seed);
+
+  color = color.clone();
+  if (!monochromatic) color.offsetHSL(0.5 / 360 * random.gaussian(), 2.5 / 100 * random.gaussian(), 2.5 / 100 * random.gaussian());
+
+  const petalScale = Random.gaussian(1, 1 / 5);
+  const petals = linspace(petalCount).map(t => {
+    const angle = Math.PI * 2 * t;
+    const o = {};
+    objectAssign(o, opts);
+    objectAssign(o, {
+      random,
+      color,
+      angle
+    });
+    const petalMesh = createPetal(o);
+    petalMesh.scale.multiplyScalar(petalScale);
+    return petalMesh;
+  });
+  const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+
+  const pitColor = color.clone();
+  if (!monochromatic) pitColor.offsetHSL(0, 0, -0.15);
+  const pit = new THREE.Mesh(sphereGeometry, createMaterial({
+    color: pitColor
+  }));
+  pit.scale.setScalar(ONE_INCH / 8);
+  pit.position.y = ONE_INCH / 5;
+  // group.add(pit);
+
+  const baseScale = ONE_INCH * random.range(0.5, 0.75);
+  const thicknes = ONE_INCH / random.range(120, 160);
+  const stamenCount = random.rangeFloor(4, 10);
+  const stamens = linspace(stamenCount).map(t => {
+    const direction = new THREE.Vector3(0, 1, 0);
+    const rotationOut = 0.075 * Math.PI;
+    direction.applyAxisAngle(new THREE.Vector3(1, 0, 0), random.gaussian() * rotationOut);
+    direction.applyAxisAngle(new THREE.Vector3(0, 0, 1), random.gaussian() * rotationOut);
+
+    const stamenLength = Math.max(0, random.gaussian(baseScale, baseScale / 4));
+    const stamenGeometry = new THREE.CylinderGeometry(thicknes, thicknes, stamenLength, 4, 1, false);
+    stamenGeometry.translate(0, stamenLength / 2, 0);
+    let newStamenColor = stamenColor.clone();
+    if (!monochromatic) newStamenColor.offsetHSL(1 / 360 * random.gaussian(), 5 / 100 * random.gaussian(), 10 / 100 * random.gaussian());
+    // newStamenColor = new THREE.Color('white')
+    const mesh = new THREE.Mesh(stamenGeometry, createMaterial({
+      color: newStamenColor
+    }));
+    mesh.quaternion.fromArray(quaternionFromDirection([], direction.toArray()));
+    mesh.position.copy(pit.position);
+    const stamen = new THREE.Group();
+    stamen.add(mesh);
+    const anther = new THREE.Mesh(sphereGeometry, mesh.material.clone());
+    anther.position.copy(mesh.position).addScaledVector(direction, stamenLength);
+    anther.scale.setScalar(ONE_INCH / 80 + random.gaussian(ONE_INCH, ONE_INCH / 4) / 160);
+    stamen.add(anther);
+    return stamen;
+  });
+
+  petals.forEach(p => group.add(p));
+  stamens.forEach(p => group.add(p));
+
+  group.position.copy(position);
+
+  // const normalArrow = new THREE.ArrowHelper(normal, position, ONE_INCH * 4, 0x0000ff);
+  // group.add(normalArrow);
+  // const tangentArrow = new THREE.ArrowHelper(tangent, position, ONE_INCH * 2, 0x00ff00);
+  // group.add(tangentArrow);
+
+  group.scale.multiplyScalar(size);
+  group.normal = normal;
+  return group;
+}
+
+function createPetalGeometry () {
+
+}
+
+function createPetal (opts = {}) {
+  let {
+    angle = 0,
+    random,
+    color = new THREE.Color('red'),
+    bloom = 1
+  } = opts;
+
+  const tangent = new THREE.Vector3(1, 0, 0);
+  const normal = new THREE.Vector3(0, 1, 0);
+
+  const padding = random.gaussian() * ONE_INCH / 40;
+  angle += random.gaussian() * Math.PI / 40;
+  const length = ONE_INCH + random.gaussian() * ONE_INCH / random.range(10, 20);
+  const width = ONE_INCH / 2 + random.gaussian() * ONE_INCH / random.range(10, 20);
+  const curlDepth = ONE_INCH / 2 + random.gaussian() * ONE_INCH / 20;
+  const zCurlAmp = ONE_INCH / 10;
+  const zCurlFreq = random.range(0.75, 1.25);
+
+  const normalAlongPlane = tangent.clone();
+  normalAlongPlane.applyAxisAngle(normal, angle);
+
+  const xSubdiv = Math.max(10, Math.round(RESOLUTION * 30));
+  const ySubdiv = Math.max(5, Math.round(xSubdiv / 2));
+  const geometry = new THREE.PlaneGeometry(1, 1, xSubdiv, ySubdiv);
+
+  geometry.vertices.forEach(vert => {
+    const u = (vert.x * 2) * 0.5 + 0.5;
+    const v = (vert.y * 2) * 0.5 + 0.5;
+    // const py = Math.sin(u * Math.PI);
+    const py = Math.pow(Math.sin(Math.pow(u, 0.95) * Math.PI), 0.75);
+
+    vert.y *= py;
+    vert.y *= 1 + Math.sin((1 - u) * Math.PI * 0.5 + Math.PI * 0.05) * 0.5;
+
+    vert.y *= width;
+    vert.x *= length;
+
+    vert.z += curlDepth + Math.sin(Math.pow(1 - u, 0.85) * -Math.PI * 0.5) * curlDepth;
+    vert.z += curlDepth * 0.5 + Math.sin(Math.pow(u, 0.5) * -Math.PI * 0.5) * curlDepth * 0.5;
+
+    vert.z += random.noise3D(u, v, angle, zCurlFreq, zCurlAmp);
+
+    vert.x += random.gaussian() * length / 75;
+    vert.y += random.gaussian() * width / 75;
+  });
+
+  geometry.rotateX(-Math.PI / 2);
+  geometry.translate(length / 2, 0, 0);
+  geometry.rotateX(random.gaussian() * Math.PI / 40);
+  geometry.rotateZ(((1 - bloom) * 90 + random.gaussian() * 10) * Math.PI / 180);
+  geometry.rotateY(angle);
+
+  geometry.computeVertexNormals();
+  // geometry.computeFlatVertexNormals();
+
+  const newColor = color.clone();
+  if (!monochromatic) newColor.offsetHSL(1 / 360 * random.gaussian(), 5 / 100 * random.gaussian(), 10 / 100 * random.gaussian());
+  const material = createMaterial({
+    color: newColor,
+    side: THREE.DoubleSide
+    // wireframe: true
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+
+  // geometry.rotateX(-Math.PI / 2);
+  // geometry.translate(length / 2, 0, 0);
+  // geometry.rotateX(random.gaussian() * Math.PI / 40);
+  // geometry.rotateZ(((1 - bloom) * 90 + random.gaussian() * 10) * Math.PI / 180);
+  // geometry.rotateY(angle);
+  
+  mesh.position.addScaledVector(normalAlongPlane, padding);
+
+  return mesh;
+  // const arrow = new THREE.ArrowHelper(direction, position, length, 0xff0000);
+  // return arrow;
+}
+
+
+function quaternionFromDirection (out = [], dir = [ 0, 1, 0 ]) {
+  // dir is assumed to be normalized
+  if (dir[1] > 0.99999) {
+    quat.set(out, 0, 0, 0, 1);
+  } else if (dir[1] < -0.99999) {
+    quat.set(out, 1, 0, 0, 0);
+  } else {
+    vec3.set(axis, dir[2], 0, -dir[0]);
+    vec3.normalize(axis, axis);
+    const radians = Math.acos(dir[1]);
+    quat.setAxisAngle(out, axis, radians);
+  }
+  return out;
+};
+
 
 class SceneAR {
 	constructor() {
@@ -22,11 +241,6 @@ class SceneAR {
 		//	debug
 		// gui.add(Config, 'heading').name('Heading AR').listen();
 
-
-		this._hasAddObject = false;
-
-		HeadingCalibrate.on('onStart', ()=>this._onCalibrateStart());
-		HeadingCalibrate.on('onEnd', ()=>this._onCalibrateEnd());
 
 		this._initHeading = 0;
 	}
@@ -59,211 +273,152 @@ class SceneAR {
 		});
 
 
-		this._heading = 0;
-		this._headingStart = 0;
-		this._headingDiff = new alfrid.EaseNumber(0, 1);
-		this._hasCalibrated = false;
-
 		XR.run({canvas:this.canvas});
 	}
 
 
-	_onCalibrateStart() {
-		console.log('Calibrating start');
-
-		this._headingStart = this._heading;
-	}
-
-
-	_onCalibrateEnd() {
-		this._hasCalibrated = true;
-		if(this._arrows1) {
-			this._arrows1.visible = true;
-			this._arrowsInitHeading.visible = false	
-		}
-
-		// console.log('End of calibration, recenter', this.headingDiff);
-		XR.XrController.recenter();
-
-
-		const { camera } = XR.Threejs.xrScene();
-		const dist = parseFloat(DebugInfo.distanceToTarget);
-		console.log('Dist to target : ', dist);
-		placeObjectInfront(this._cube2, camera, this._heading + this.headingDiff, dist, true);
-	}
-
-
 	_onStart() {
-		const { camera, scene } = XR.Threejs.xrScene();
-		this._initHeading = Device.heading;
+		const { camera, scene, renderer } = XR.Threejs.xrScene();
+		camera.position.set(0, AVG_HUMAN_HEIGHT, 0);
 
-		// Add a light to the scene
-		const light = new THREE.AmbientLight( 0x404040, 5 ); // soft white light
-		scene.add(light)
+		
 
-		const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-		scene.add( directionalLight );
-		directionalLight.position.x = directionalLight.position.z = .2;
 
-		// Set the initial camera position relative to the scene we just laid out. This must be at a
-		// height greater than y=0.
-		camera.position.set(0, AVG_HUMAN_HEIGHT, 0)
+		const petalGeometryCache = [];
+		const maxPetalGeometries = 20;
+		
+		const defaultStamenColor = 'hsl(0, 0%, 100%)';
+		let palette = Random.pick(palettes);
+		const nColors = Random.rangeFloor(4, 5);
+		palette = Random.shuffle(palette).slice(0, nColors);
+		const branchColor = palette[1];
+		renderer.setClearColor('#fff', 1);
 
-		// Sync the xr controller's 6DoF position and camera paremeters with our scene.
-		XR.XrController.updateCameraProjectionMatrix({
-			origin: camera.position,
-			facing: camera.quaternion,
+		const meshes = new THREE.Group();
+		this.meshes = meshes;
+		let s = 8;
+		this.meshes.scale.set(s, s, s);
+		this.meshes.position.set(0, 0, -0.5);
+		scene.add(meshes);
+
+		const ground = new THREE.Mesh(
+		  new THREE.PlaneGeometry(1, 1, 5, 5),
+		  new THREE.MeshBasicMaterial({
+		    color: 'hsl(0, 0%, 90%)',
+		    wireframe: true
+		  })
+		);
+		ground.geometry.rotateX(-Math.PI / 2);
+		// scene.add(ground);
+
+		const normal = new THREE.Vector3(0, -1, 0);
+		const length = ONE_INCH * 12;
+		const start = new THREE.Vector3();
+		const end = new THREE.Vector3().addScaledVector(normal, -length);
+		// const start = new THREE.Vector3().addScaledVector(normal, length / 2);
+		// const end = new THREE.Vector3().addScaledVector(normal, -length / 2);
+		const steps = 10;
+		const vertices = linspace(steps, true).map(t => {
+		  const position = start.clone().lerp(end, t);
+		  const direction = new THREE.Vector3().fromArray(Random.onSphere());
+		  const scale = Random.gaussian() * ONE_INCH / 2;
+		  return position.addScaledVector(direction, scale);
 		});
+		const curve = new THREE.CatmullRomCurve3(vertices);
+		const tubeRadius = ONE_INCH * 0.015;
+		const tube = new THREE.TubeGeometry(curve, 20, tubeRadius, 8, false);
+		const branch = new THREE.Mesh(tube, createMaterial({
+		  color: branchColor
+		}));
+		meshes.add(branch);
 
-		XR.XrController.configure({ enableWorldPoints: true });
+		const tangentCurve = new THREE.CatmullRomCurve3(tube.tangents);
+		const normalCurve = new THREE.CatmullRomCurve3(tube.normals);
+		const binormalCurve = new THREE.CatmullRomCurve3(tube.normals);
 
-		for (let i = -5; i <=5 ; i += .5) {
-			for (let j = -5; j <= 5; j += .5) {
-				if (Math.round(i) != i && Math.round(j) != j) { continue }
-				const sphere = new THREE.Mesh(
-					new THREE.SphereGeometry(.03, 8, 8), new THREE.MeshBasicMaterial({color: purple}))
-				sphere.position.set(i, 0, j)
-				scene.add(sphere)
-			}
+
+		const flowers = linspace(flowerCount, true).map((t) => {
+		  if (t < 0.5 && Random.gaussian() > 0) return false;
+		  if (Random.gaussian() > 0) t = clamp01(t + Random.gaussian() / 40);
+		  const position = curve.getPointAt(t);
+		  const tangent = tangentCurve.getPoint(t).normalize();
+		  const binormal = binormalCurve.getPoint(t).normalize();
+		  const originalNormal = normalCurve.getPoint(t).normalize();
+		  const normal = tangent.clone();
+		  normal.applyAxisAngle(binormal, Math.PI / 2 + Random.gaussian() * Math.PI * 0.1);
+		  normal.applyAxisAngle(tangent, Random.range(-1, 1) * Math.PI);
+		  // normal.applyAxisAngle(tangent, Random.range(-1, 1) * Math.PI * 2);
+		  // const position = new THREE.Vector3().fromArray(Random.insideSphere(ONE_INCH * 4));
+		  // const normal = new THREE.Vector3().fromArray(Random.onSphere());
+		  const colorDeck = Random.shuffle(palette);
+
+		  const stamenColor = defaultStamenColor || (monochromatic ? new THREE.Color(branchColor) : new THREE.Color(colorDeck[1] || colorDeck[0]));
+		  const flower = createFlower({
+		    position: new THREE.Vector3(),
+		    normal,
+		    color: new THREE.Color(colorDeck[0]),
+		    stamenColor: new THREE.Color(stamenColor),
+		    petalCount: Random.rangeFloor(3, 6),
+		    bloom: Random.range(0.5, 1)
+		  });
+		  let size = Random.gaussian(1, 1 / 4) * 0.5;
+		  if (Random.gaussian() > 2) size += Random.gaussian() * 0.1;
+		  if (Random.gaussian() > 1) size += Random.gaussian() * 0.1;
+		  flower.scale.multiplyScalar(size);
+		  const group = new THREE.Group();
+		  group.t = t;
+		  group.time = 0;
+		  group.duration = 1;
+		  group.position.copy(position);
+		  // flower.position.addScaledVector(normal, Math.abs(Random.gaussian()) * ONE_INCH * 0.25);
+		  // group.position.addScaledVector(normal, Math.abs(Random.gaussian() * ONE_INCH * 0.15));
+		  group.add(flower);
+		  meshes.add(group);
+
+		  flower.quaternion.fromArray(quaternionFromDirection([], normal.toArray()));
+
+		  return group;
+		}).filter(Boolean);
+
+		this.flowers = flowers;
+
+		this.time = new Date().getTime();
+
+		const begin = () => {
+		  this.flowers.forEach((flower, i) => {
+		    flower.time = 0;
+		    flower.scale.setScalar(1e-5);
+		  });
 		}
 
-		//	loading chevron model
-		loadModel('./assets/gltf/arrows.gltf')
-			.then( mArrow => this._addArrows(mArrow), (e)=> {
-				console.log('Error :', e);
-			});
-
-
-		const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-		const material = new THREE.MeshStandardMaterial( {
-			roughness:1.0,
-			metalness:0.5,
-			color:0x1122FF
-		} );
-
-		const material2 = new THREE.MeshStandardMaterial( {
-			roughness:1.0,
-			metalness:0.5,
-			color:0x11FF22
-		} );
-		this._cube = new THREE.Mesh( geometry, material );
-		this._cube2 = new THREE.Mesh( geometry, material2 );
-		scene.add( this._cube );
-		scene.add( this._cube2 );
+		begin();
+		setInterval(begin, 5000);
 	}
 
-
-	placeObject() {
-		const { camera } = XR.Threejs.xrScene();
-		if(!this._hasAddObject) {
-			console.log('Place object ');
-
-			placeObjectInfront(this._cube, camera, this._heading, 10, true);
-		}
-		this._hasAddObject = true;
-	}
-
-	removeObject() {
-		this._hasAddObject = false;
-	}
-
-	_addArrows(mArrow) {
-		const { camera, scene } = XR.Threejs.xrScene();
-		const scaleArrow = 0.25;
-
-		this._arrows = mArrow;
-		this._arrows.scale.set(scaleArrow, scaleArrow, scaleArrow);
-		let meshes = this._arrows.children;
-		const material = new THREE.MeshStandardMaterial({
-			roughness:1.0,
-			metalness:0.5,
-			color:0xFF6600
-		});
-
-		meshes.forEach( mesh => {
-			mesh.material = material;
-		});
-		scene.add( this._arrows );
-
-		this._arrows1 = mArrow.clone();
-		this._arrows1.scale.set(scaleArrow+.1, scaleArrow+.1, scaleArrow+.1);
-		const meshes2 = this._arrows1.children;
-		const material2 = new THREE.MeshStandardMaterial({
-			roughness:1.0,
-			metalness:0.5,
-			color:0x00FF66
-		});
-
-		meshes2.forEach( mesh => {
-			mesh.material = material2;
-		});
-		scene.add( this._arrows1 );
-		this._arrows1.visible = false;
-
-
-		this._arrowsInitHeading = mArrow.clone();
-		this._arrowsInitHeading.scale.set(scaleArrow+.1, scaleArrow+.1, scaleArrow+.1);
-		meshes = this._arrowsInitHeading.children;
-		const materialInit = new THREE.MeshStandardMaterial({
-			roughness:1.0,
-			metalness:0.5,
-			color:0x999999
-		});
-		// console.log('materialInit', materialInit);
-
-		meshes.forEach( mesh => {
-			mesh.material = materialInit;
-		});
-		scene.add( this._arrowsInitHeading );
-
-	}
 
 
 	_onUpdate() {
-		const { camera } = XR.Threejs.xrScene();
-		let front = new THREE.Vector3(0, 0, -1);
+		const t = new Date().getTime();
+		const deltaTime = t - this.time;
+		this.time = t;
+		const { camera, scene } = XR.Threejs.xrScene();
+		scene.scale.set(2, 2, 2);
 
-		const {x, z} = camera.position;
-		front.applyQuaternion(camera.quaternion);
-		front.setLength(10);
-		this._heading = Math.atan2(front.z, front.x) + Math.PI/2;
+		this.flowers.forEach((flower, i) => {
+		  flower.time += deltaTime/1000;
+		  const delay = i * 0.05;
+		  // const delay = flower.t * 1;
+		
+		  const time = Math.max(0, flower.time - delay);
+		  
+		  const duration = flower.duration;
+		  let scale = clamp01(time / duration);
+		  scale = eases.circOut(scale);
+		  scale = Math.max(1e-5, scale);
 
-		front.setLength(2);
-		front.add(camera.position);
-
-		if(this._arrows) {
-			this._arrows.position.copy(front);	
-			this._arrows.rotation.y = -this._heading;
-		}
-
-
-		let heading2 = this._heading + this.headingDiff;
-		front.x = 0;
-		front.y = 0;
-		front.z = -2;
-
-		front.applyAxisAngle(new THREE.Vector3(0, -1, 0), heading2);
-		front.add(camera.position);
-		front.y -= 0.5;
-
-		if(this._arrows1) {
-			placeObjectInfront(this._arrows1, camera, heading2, 3, true);
-		}
-
-		if(!this._hasCalibrated && this._arrowsInitHeading) {
-			let heading = this._heading + ( this._initHeading - Device.headingLocal);
-			placeObjectInfront(this._arrowsInitHeading, camera, heading, 3, true);
-		}
-
-	}
-
-
-	get headingDiff() {
-		return this._headingDiff.value;
-	}
-
-	set headingDiff(mValue) {
-		this._headingDiff.value = mValue;
+		  flower.scale.setScalar(scale);
+		  // flower.rotation.x = playhead * Math.PI * 0.05;
+		});
 	}
 
 }
--- src/js/app.js
+++ src/js/app.js
@@ -2,17 +2,11 @@ import '../scss/global.scss';
 import debugPolyfill from './debug/debugPolyfill';
 import alfrid, { GL } from 'alfrid';
 
-import GoogleMapsLoader from 'google-maps';
-import PathTracker from './utils/PathTracker';
-import HeadingCalibrate from './utils/HeadingCalibrate';
-import DebugInfo from './debug/DebugInfo';
-import DebugCanvas from './debug/DebugCanvas';
 import SceneAR from './SceneAR';
 import Config from './Config';
 import * as THREE from 'three';
 
-const GOOGLE_MAP_API_KEY = 'AIzaSyBqCqukoHGzJjI7Sqo41Nw9XT0AhnGoVDw';
-window.THREE =  THREE;
+window.THREE = THREE
 
 
 import { fromLatLngToPixel, distanceLatLng, directionLatLng, directionMapPoint } from './utils';
@@ -23,56 +17,11 @@ if(document.body) {
 	window.addEventListener('DOMContentLoaded', _init);
 }
 
-const zoom = 16;
-let map, marker, markerTarget1, markerTarget2, markerStart;
- 
-let target1 = {
-	lat:51.52864213850285,
-	lng:-0.08503963359066802
-}
-
-let target2 = {
-	lat:51.52790792006495,
-	lng:-0.08365561373898345
-}
-
-const oDebug = {
-	latitude:'0',
-	longitude:'0',
-	heading:'0',
-	headingLocal:'0',
-	alpha:'0',
-	headingOffset:'0',
-}
-
-
 
 let sceneAR, debugCanvas;
-let point = {
-	x:0, y:0
-}
-let target = {
-	x:0, y:0
-}
-let heading = 0;
-let headingLocal = 1.;
-let headingOffset = 0;
-let headingTarget = 0;
-let hasCalibrated = false;
 
-let locPrev = {lat: 51.528111499999994, lng: -0.0859945};
-let locCurr = {lat: 51.528111499999994, lng: -0.0859945};
-
-let _fake = 0;
-let myLoc = {
-	lat:0,
-	lng:0
-}
-
-function _initMap() {
-
+function _init() {
 	const onxrloaded = () => {
-		console.log('XR:', window.XR);
 		sceneAR = new SceneAR();
 	}
 
@@ -81,210 +30,7 @@ function _initMap() {
 	} else {
 		window.addEventListener('xrloaded', onxrloaded)
 	}
-
-
-	debugCanvas = new DebugCanvas();
-	DebugInfo.headingLocal = headingLocal;
-
-	markerTarget1 = new google.maps.Marker({
-		position: target1,
-		map: map,
-		title: 'Target 1'
-	});
-
-	let time = new Date().getTime();
-	console.log(navigator.geolocation.watchPosition);
-
-	navigator.geolocation.watchPosition((o)=> {
-		console.log('Position Upate : ', o.coords);
-		let t = new Date().getTime();
-		DebugInfo.geoUpdateInterval = `${t - time}`;
-		time = t;	
-	})
-
-	const updateLocation = () => {
-
-		if (navigator.geolocation) {
-			
-			navigator.geolocation.getCurrentPosition( (o)=> {
-
-				oDebug.latitude = o.coords.latitude.toString();
-				oDebug.longitude = o.coords.longitude.toString();
-				if(o.coords.latitude !== myLoc.lat && o.coords.longitude !== myLoc.lng) {
-					// console.log('Geolocation update:', o.coords);
-					// let t = new Date().getTime();
-					// DebugInfo.geoUpdateInterval = `${t - time}`;
-					// time = t;	
-					// myLoc.lat = o.coords.latitude;
-					// myLoc.lng = o.coords.longitude;
-				}
-
-
-				const myLatlng = {
-					lat: o.coords.latitude, 
-					lng: o.coords.longitude + _fake
-				};
-				locPrev.lat = locCurr.lat;
-				locPrev.lng = locCurr.lng;
-				locCurr.lat = myLatlng.lat;
-				locCurr.lng = myLatlng.lng;
-
-				if(!marker) {
-					marker = new google.maps.Marker({
-						position: myLatlng,
-						map: map,
-						title: 'Me'
-					});	
-				} else {
-					const loc = new google.maps.LatLng(myLatlng);
-					marker.setPosition(loc);
-				}
-
-				const distToTarget = distanceLatLng(myLatlng, target1);
-				DebugInfo.distanceToTarget = `${distToTarget}`;
-
-				if(distToTarget < 10) {
-					DebugInfo._state = 'Adding Object';
-					sceneAR.placeObject();
-				}
-
-				target = fromLatLngToPixel(map, markerTarget1.position);
-				point = fromLatLngToPixel(map, marker.position);
-				DebugInfo.point = point;
-
-
-				//	get heading to target
-				const headingTarget = directionMapPoint(point, target) + Math.PI/2;
-				DebugInfo.headingTarget = headingTarget;
-				let headingDiff = headingTarget - (headingLocal + HeadingCalibrate.offset);
-				if(sceneAR) {
-					sceneAR.headingDiff = headingDiff;	
-				}
-
-				alfrid.Scheduler.next(updateLocation);
-
-				if(!GL.isMobile) {
-					// _fake += 0.00005;
-					const t = new Date().getTime() * 0.001;
-					_fake = Math.sin(t) * 0.0015;
-				}
-
-			} );
-
-		}
-
-	}
-
-	updateLocation();
-
-	const options = {enableHighAccuracy: true,timeout: 500, maximumAge: 0,desiredAccuracy: 0 };
-
-	const oControls = {
-		toggleMinified:() => {
-			const mapDiv = document.body.querySelector('#map');
-			if(document.body.classList.contains('minified')) {
-				document.body.classList.remove('minified');
-				mapDiv.style.marginTop = '0px';
-				debugCanvas.canvas.height = window.innerHeight;
-				debugCanvas.canvas.style.marginTop = '0px';
-			} else {
-				document.body.classList.add('minified');
-				const top = Math.floor(window.innerHeight * (1.0 - Config.minifiedHeight));
-				mapDiv.style.marginTop = `${top}px`;
-				debugCanvas.canvas.height = Math.floor(window.innerHeight * Config.minifiedHeight);
-				debugCanvas.canvas.style.marginTop = `${top}px`;
-			}
-
-			google.maps.event.trigger(map, 'resize')
-		}
-	}
-
-
-	setTimeout(()=> {
-		gui.add(DebugInfo, 'distanceToTarget').name('Distance to Target').listen();
-		gui.add(DebugInfo, 'state').listen();
-		// gui.add(DebugInfo, 'geoUpdateInterval').listen();
-		// gui.add(HeadingCalibrate, 'stateString').listen();
-		// gui.add(oControls, 'toggleMinified');
-	}, 200);
-
-
-	window.addEventListener('deviceorientation', function(event) {
-		headingLocal = -event.alpha * Math.PI / 180;
-		if(!GL.isMobile) {
-			headingLocal = 0.5;
-		}
-
-		DebugInfo.headingLocal = headingLocal;
-
-	}, false);
-
-
-	const btnCalibre = document.body.querySelector('.btn-calibrate');
-	btnCalibre.addEventListener('click', (e)=> {
-
-		if(!hasCalibrated) {
-			btnCalibre.innerHTML = 'CALIBRATING...';
-			hasCalibrated = true;
-
-			oControls.toggleMinified();
-
-			HeadingCalibrate.once('onStart', loc => {
-				markerStart = new google.maps.Marker({
-					position: loc,
-					map: map,
-					title: 'Target 1'
-				});
-
-				DebugInfo.state = 'Calibrating';
-			});
-
-
-			HeadingCalibrate.on('onProgress', percent => {
-				console.log('Calibrating progress :', percent);
-			});
-
-			HeadingCalibrate.start()
-			.then((offset)=> {
-				oDebug.headingOffset = `${HeadingCalibrate.offset}`;
-				document.body.classList.add('hasCalibrated');
-				DebugInfo.headingOffset = HeadingCalibrate.offset;
-
-				DebugInfo.state = 'Calibrating End';
-			}, (e)=> {
-				console.log('Error', e);
-			});
-		} 
-
-	});
-
-	/*/
-	console.log('localhost ? ', window.location.href.indexOf('localhost') > -1);
-	if(window.location.href.indexOf('localhost') > -1) {
-		document.body.classList.add('hasCalibrated');
-		oControls.toggleMinified();
-	}
-	//*/
-}
-
-function loop() {
-
-}
-
-
-
-function _init() {
-	GoogleMapsLoader.KEY = GOOGLE_MAP_API_KEY;
-	const el = document.getElementById('map');
-
-	GoogleMapsLoader.load((_google) => {
-		map = new google.maps.Map(el, {
-			center: {lat: 51.528111499999994, lng: -0.0859945},
-			zoom
-		});
-
-		_initMap();
-	});
+	
 
 }
 
